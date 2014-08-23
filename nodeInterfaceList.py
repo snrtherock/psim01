28f7dc90e41a6034bea3e6c53dd9e4f71275e696
Christopher Upkes <cupkes@stratatron.com>
2014-08-23 13:55:22 -0700

    added file nodeInterfaceList.py for nodeInterfaceList class

---
+++ nodeInterfaceList.py
@@ -0,0 +1,30 @@
+# file: wrapper class for list of interface object
+# Author:  Christopher Upkes
+# Owner: Stratatron, LLLC
+#
+class NodeInterfaceList( object ) :
+    def _init_( self, interfaceList=None) :
+        self._interfaces = interfaceList
+
+    @property
+    def interfaces( self ) :
+        retrn self._interfaces
+
+#    @interfaces.setter
+#    def interfaces( self, value) :
+#        self._interfaces = value
+
+    @interfaces.deleter
+    def interfaces( self ) :
+        del self.interfaces
+
+    def removeInterface( self, value ) :
+        self._interfaces.remove(value) # add exception handling
+
+    def addInterface( self, value ) :
+        self._interfaces.append(value) # add exception handling
+
+        
+        
+        
+        
