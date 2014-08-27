ad00a576422d5efdeae15dd37a7d46ff24dd7a9a
Christopher Upkes <cupkes@stratatron.com>
2014-08-27 15:26:02 -0700

    modified fileScanner and scantest

--- fileScanner.py
+++ fileScanner.py
@@ -6,10 +6,21 @@ Created on Mon Aug 25 11:38:05 2014
 @owner: Stratatron,LLC
 
 """
-def fileScaner(fileName, function):
+class UnknownToken(Exception) : pass
+    
+def processLine(line):
+    if line[0] != 'x':
+        print("success")
+    else:
+        raise UnknownToken(line)
+def fileScanner(fileName, function):
     file = open(fileName, 'r')
     while True:
         line = file.readline()
+        print(line)
         if not line: break
         function(line)
     file.close()
+    
+filename = '../psim01/data/data.txt'
+fileScanner(filename, processLine)
--- scantest.py
+++ scantest.py
@@ -10,12 +10,15 @@ Created on Wed Aug 27 13:06:28 2014
 
 class UnknownCommand(Exception) : pass
     
+#==============================================================================
+# TO_DO add code to process the line byte by byte and extract all data
+#==============================================================================
 def processLine(line):
     if line[0] == 'x':
         print("success")
     else:
         raise UnknownCommand(line)
-def fileScaner(fileName, function):
+def fileScanner(fileName, function):
     file = open(fileName, 'r')
     while True:
         line = file.readline()
@@ -23,5 +26,5 @@ def fileScaner(fileName, function):
         function(line)
     file.close()
     
-filename = '../data/data.txt'
+filename = './data/AllDiskInfo.txt'
 fileScanner(filename, processLine)
