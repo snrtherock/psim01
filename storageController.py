25ca398a5ac39f0780d53ff325440205118a9176
Christopher Upkes <cupkes@stratatron.com>
2014-08-28 12:47:29 -0700

    Added storageController class file and model library file.  Updated scantest

---
+++ storageController.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 28 12:34:06 2014
+@description:  
+@author: christopherupkes
+@owner: Stratatron,LLC
+
+"""
+
+class StorageController :
+    def _init_( self ):
+        None
+#==============================================================================
+# TO_DO add initialization code here.
+#==============================================================================
---
+++ storagesystemmodel.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 28 12:35:16 2014
+@description: storagesystemmodel model library file
+@author: christopherupkes
+@owner: Stratatron,LLC
+
+"""
+#==============================================================================
+#  Network interface wrapper class
+#==============================================================================
+class Interface( object ) :
+    def _init_(self, name, ifType, physName, address=None) :
+        self._name = name
+        self._ip4_address = address
+        self._physName = physName
+        self._ifType
+        
+    @property
+    def ip4_address(self) :
+        return self._ip4_address
+
+    @ip4_address.setter
+    def ip4_address(self, value) :
+        self._ip4_address = value
+
+    @ip4_address.deleter
+    def ip4_address(self) :
+        del self._ip4_address
+
+
+    @property
+    def name(self) :
+        return self._name
+
+    @name.setter
+    def name(self, value) :
+        self._name = value
+
+    @name.deleter
+    def name(self) :
+        del self._name
+        
+
+    @property
+    def ifType(self) :
+        return self._ifType
+
+    @ifType.setter
+    def ifType(self, value) :
+        self._ifType = value
+
+    @ifType.deleter
+    def ifType(self) :
+        del self._ifType
+
+
+    @property
+    def physName(self) :
+        return self._physName
+
+    @physName.setter
+    def physName(self, value) :
+        self._physName = value
+
+    @physName.deleter
+    def physName(self) :
+        del self._physName
+        
+#==============================================================================
+#  Network interface list collection wrapper class
+#==============================================================================
+
+class NodeInterfaceList( object ) :
+    def _init_( self, interfaceList) :
+        self._interfaces = interfaceList
+
+    @property
+    def interfaces( self ) :
+        return self._interfaces
+
+#    @interfaces.setter
+#    def interfaces( self, value) :
+#        self._interfaces = value
+
+    @interfaces.deleter
+    def interfaces( self ) :
+        del self.interfaces
+
+    def removeInterface( self, value ) :
+        self._interfaces.remove(value) # add exception handling
+
+    def addInterface( self, value ) :
+        self._interfaces.append(value) # add exception handling
+
+#==============================================================================
+#  JbodNode model class
+#==============================================================================
+
+class jbodNode :
+    def _init_( self, wwn, capacity, mfgr, nodeType ) :
+        self._wwn = wwn
+        self._capacty = capacity
+        self._mfgr = mfgr
+        self._nodeType = nodeType
+
+    @property
+    def nodeId( self ) :
+        return self._wwn
+
+    @property
+    def capacity( self ) :
+        return self._capacity
+
+    @property
+    def mfgr( self ) :
+        return self._mfgr
+
+    @property
+    def nodeType( self ) :
+        return self._nodeType
+        
+#==============================================================================
+# jbodNode collection wrapper class
+#==============================================================================
+
+class JbodNodeList( object ) :
+    def _init_( self , nodes) :
+        self._nodes = nodes
+
+    @property
+    def nodes( self ) :
+        return self._nodes
+
+    def addNode( self, value ) :
+        self._nodes.append(value)
+
+    def removeNode( self, value ) :
+        self._nodes.remove(value)
+        
+#==============================================================================
+#  clusterNode model classs
+#==============================================================================
+
+class ClusterNode :
+    def _init_( self, number, nodeID, name, nodeInterfaceList=None, peer=None ) :
+        self._number = number
+        self._nodeId = nodeID
+        self._name = name
+        self._nodeInterfaceList = nodeInterfaceList
+        self._peer = peer
+        
+    @property
+    def number( self ) :
+        return self._number
+    
+    @property
+    def nodeId( self ) :
+        return self._nodeID
+        
+    @property
+    def name( self ) :
+        return self._name
+        
+    @property
+    def nodeInterfaceList( self ) :
+        return self._nodeInterfaceList
+        
+    @property
+    def peer( self ) :
+        return self._peer
+        
+    @peer.setter
+    def peer( self, value ) :
+        self._peer = value
+    
+    @peer.deleter
+    def peer( self ) :
+        del self._peer
+        
+    def validateHeartbeat( self ) :
+        None
+        # TO_DO validate heartbeat ips to ensure in same subnet
+        
+#==============================================================================
+#  cluster model class
+#==============================================================================
+
+class Cluster :
+    def _init_( self, nodeList, name ) :
+        self._nodeList = nodeList
+        self._name = name
+        
+    @property
+    def nodeList( self ) :
+        return self._nodeList
+    
+    def addNode( self, node ) :
+        self._nodeList.append( node )
+    
+    def removeNode( self, node ) :
+        self._nodeList.remove( node )
+
+#==============================================================================
+#  Storage Controller model class
+#==============================================================================
+
+
+
+
